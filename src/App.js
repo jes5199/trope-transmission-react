ba6b8f8c0e86e899497c2df8192a58ed7cc8f015
Jes Wolfe! <jes5199@gmail.com>
2022-05-15 22:30:06 -0700

    experiment with clickable words

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import './App.css';
 import DECTalk from './Views/DECTalk';
 import TropeCatalogue from './Views/TropeCatalogue.js';
+import VoicedWord from './Views/VoicedWord';
 
 function App() {
   //const [tropText, setTropText] = useState("טִפְחָ֖א");
+  const [selectedWord, setSelectedWord] = useState(null);
+
+  const audioRef = useRef();
+
+  const decTalkTexts = [
+    "hello",
+    "test"
+  ];
+
+  const decTalkText = selectedWord === null ? "" : decTalkTexts[selectedWord]
 
   return (
     <div className="App">
-      <DECTalk value="hello" />
-      <TropeCatalogue/>
+      <div style={{fontSize: 48}}>
+      <VoicedWord color="pink" selected={selectedWord === 0} onClick={() => setSelectedWord(0)} onDoubleClick={() => audioRef.current?.play()} >טִפְחָ֖א</VoicedWord>
+      <VoicedWord color="pink" selected={selectedWord === 1} onClick={() => setSelectedWord(1)} onDoubleClick={() => audioRef.current?.play()}>טִפְחָ֖א</VoicedWord>
+      </div>
+
+      <div>
+        <DECTalk value={decTalkText} audioRef={audioRef}/>
+      </div>
     </div>
   );
 }
