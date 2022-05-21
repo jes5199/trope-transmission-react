3adca39cbaf18e657de096ca788d4a7acee9b0cc
Jes Wolfe! <jes5199@gmail.com>
2022-05-21 15:33:03 -0700

    talking!

---
+++ src/Views/DECTalk.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function DECTalk({value, audioRef}) {
+    const [oldText, setOldText] = useState(value);
+    const [text, setText] = useState(value);
+
+    if(oldText !== value) {
+        setTimeout(() => {setOldText(value); setText(value)}, 10);
+    }
+
+    const audioUrl = text === "" ? "" : `http://34.222.28.30/tropetalk?say=${encodeURIComponent(text)}`;
+
+    const handleChange = event => {
+        console.log("setting text to ", event.target.value);
+        setText(event.target.value);
+    };
+
+    return <div style={{margin: "20px"}} id={"decTalk " + value}>
+        <div>
+            <textarea value={text} onChange={handleChange}></textarea>
+        </div>
+        { audioUrl &&
+        <div>
+            <audio controls src={audioUrl} ref={audioRef}>
+            <source src={audioUrl} type="audio/wav" />
+            </audio>
+        </div>
+        }
+    </div>
+}
+
+export default DECTalk;
---
+++ src/Views/VoicedWord.js
@@ -0,0 +1,26 @@
+function cssColor(color) {
+    if (color == "pink") {
+        return "#FE70FF"
+    }
+}
+
+function VoicedWord({value, color, selected, onClick, onDoubleClick, children}) {
+    const styleColor = cssColor(color);
+    const selectedColor = "#808080";
+
+    const backgroundColor = selected ? selectedColor : styleColor;
+    const textColor = selected ? styleColor : "#000000";
+
+
+    return <span style={{
+        backgroundColor: backgroundColor,
+        color: textColor,
+        fontSize: 48, padding: "5px", margin: "15px"}}
+        onClick={onClick}
+        onDoubleClick={onDoubleClick}>
+        {children}
+    </span>
+
+}
+
+export default VoicedWord;
